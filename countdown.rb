952d88ae8464d8d7efaef483ee4e81043087494e
Joan Manuel Araujo <WISDOMBTHEGAME@GMAIL.COM>
2016-01-30 13:43:43 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,37 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  it_is =  "HAPPY NEW YEAR!"
+  while number != 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  it_is
 end
+def countdown_with_sleep(number)
+  it_is =  "HAPPY NEW YEAR!"
+  while number != 0
+    puts "#{number} SECOND(S)!"
+    sleep (1)
+    number -= 1
+  end
+end
+
+
+
+
+
+
+#phrase = "Welcome to Flatiron School's Web Development Course!"
+ # suny = 0
+ # while suny < number_of_times do
+  #  puts phrase
+  #  suny +=1
+
+#sleep(4.minutes)
+
+# x = 1
+#while x < 10
+ # puts "#{x} is less than 10"
+  #x += 1
+#end
